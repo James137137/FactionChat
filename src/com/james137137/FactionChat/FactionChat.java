bd9ed44380ab1a9a0e5ed81360845cfd0ef664a4
James Anderson <james137137@lolnet.co.nz>
2013-06-13 18:09:33 +1200

    Version 1.52701

--- src/com/james137137/FactionChat/FactionChat.java
+++ src/com/james137137/FactionChat/FactionChat.java
@@ -4,7 +4,6 @@
  */
 package com.james137137.FactionChat;
 
-import com.james137137.advertiser.Advertiser;
 import com.james137137.mcstats.Metrics;
 import java.io.File;
 import java.io.IOException;
@@ -38,7 +37,6 @@ public class FactionChat extends JavaPlugin {
             ModChatEnable, AdminChatEnable, JrModChatEnable, SrModChatEnable, JrAdminChatEnable, UAChatEnable;
     private int reloadCountCheck = 0;
     public static boolean FactionsEnable;
-    private Advertiser advertiser;
 
     @Override
     public void onEnable() {
@@ -69,13 +67,7 @@ public class FactionChat extends JavaPlugin {
         {
             Updater updater = new Updater(this, "factionchat", this.getFile(), Updater.UpdateType.DEFAULT, false);
         }
-        advertiser = new Advertiser(this);
-        boolean checkClassIsNotDeleted = false;
-        checkClassIsNotDeleted = advertiser.check();
-        if (!checkClassIsNotDeleted)
-        {
-            Bukkit.getServer().getPluginManager().disablePlugin(this);
-        }
+        
 
 
 
@@ -91,7 +83,6 @@ public class FactionChat extends JavaPlugin {
     @Override
     public void onDisable() {
         log.log(Level.INFO, "{0}: disabled", this.getName());
-        advertiser.StopAdvertiser();
     }
 
     protected void removeConfigFile() {
