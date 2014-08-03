d2dbb268eae9dd503f21e6d96c884102ac3dddaa
James137137 <james137137@gmail.com>
2014-08-03 14:49:20 +1200

    updated FactionChatListener2 to match with FactionChatListener

--- src/main/java/nz/co/lolnet/james137137/FactionChat/FactionChatListener2.java
+++ src/main/java/nz/co/lolnet/james137137/FactionChat/FactionChatListener2.java
@@ -1,6 +1,7 @@
 package nz.co.lolnet.james137137.FactionChat;
 
 import java.util.Objects;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
@@ -49,30 +50,49 @@ public class FactionChatListener2 implements Listener {
         onPlayerJoinEP = setupEventPriority("EventPriority.onPlayerJoin");
 
         PluginManager pm = Bukkit.getPluginManager();
-        pm.registerEvent(AsyncPlayerChatEvent.class, this, onPlayerChatEP, new EventExecutor() {
+        if (!plugin.getConfig().getBoolean("DontUseAsyncEvent")) {
+            pm.registerEvent(org.bukkit.event.player.AsyncPlayerChatEvent.class, this, onPlayerChatEP, new EventExecutor() {
 
-            @Override
-            public void execute(Listener ll, Event event) throws EventException {
-                FactionChatListener2.this.onPlayerChat( (AsyncPlayerChatEvent) event);
-            }
-        }, plugin);
-        
-        pm.registerEvent(AsyncPlayerChatEvent.class, this, onPlayerChatLocalOptionEP, new EventExecutor() {
+                @Override
+                public void execute(Listener ll, Event event) throws EventException {
+                    FactionChatListener2.this.onPlayerChat((org.bukkit.event.player.AsyncPlayerChatEvent) event);
+                }
+            }, plugin);
 
-            @Override
-            public void execute(Listener ll, Event event) throws EventException {
-                FactionChatListener2.this.onPlayerChatLocalOption( (AsyncPlayerChatEvent) event);
-            }
-        }, plugin);
-        
+            pm.registerEvent(org.bukkit.event.player.AsyncPlayerChatEvent.class, this, onPlayerChatLocalOptionEP, new EventExecutor() {
+
+                @Override
+                public void execute(Listener ll, Event event) throws EventException {
+                    FactionChatListener2.this.onPlayerChatLocalOption((org.bukkit.event.player.AsyncPlayerChatEvent) event);
+                }
+            }, plugin);
+
+        } else {
+            pm.registerEvent(org.bukkit.event.player.PlayerChatEvent.class, this, onPlayerChatEP, new EventExecutor() {
+
+                @Override
+                public void execute(Listener ll, Event event) throws EventException {
+                    FactionChatListener2.this.onPlayerChat_NonAsync((org.bukkit.event.player.PlayerChatEvent) event);
+                }
+            }, plugin);
+
+            pm.registerEvent(org.bukkit.event.player.PlayerChatEvent.class, this, onPlayerChatLocalOptionEP, new EventExecutor() {
+
+                @Override
+                public void execute(Listener ll, Event event) throws EventException {
+                    FactionChatListener2.this.onPlayerChatLocalOption_NonAsync((org.bukkit.event.player.PlayerChatEvent) event);
+                }
+            }, plugin);
+        }
+
         pm.registerEvent(PlayerCommandPreprocessEvent.class, this, onPlayerCommandEP, new EventExecutor() {
 
             @Override
             public void execute(Listener ll, Event event) throws EventException {
-                FactionChatListener2.this.onPlayerCommand( (PlayerCommandPreprocessEvent) event);
+                FactionChatListener2.this.onPlayerCommand((PlayerCommandPreprocessEvent) event);
             }
         }, plugin);
-        
+
         pm.registerEvent(PlayerJoinEvent.class, this, onPlayerJoinEP, new EventExecutor() {
 
             @Override
@@ -86,21 +106,33 @@ public class FactionChatListener2 implements Listener {
         Player player = event.getPlayer();
         ChatMode.SetNewChatMode(player);
     }
-
-    protected void onPlayerChat(AsyncPlayerChatEvent event) {
+    
+    protected void onPlayerChat(org.bukkit.event.player.AsyncPlayerChatEvent event) {
 
         if (event.isCancelled()) {
             return;
         }
-        Player talkingPlayer = event.getPlayer();
-        if (FactionChat.useBanManager())
-        {
-            if (BanManagerAPI.isMuted(talkingPlayer.getName()))
-            {
-                return;
+        boolean cancelEvent = onChat(event.getPlayer(), event.getMessage(), event.getRecipients());
+        event.setCancelled(cancelEvent);
+
+    }
+
+    private void onPlayerChat_NonAsync(org.bukkit.event.player.PlayerChatEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        boolean cancelEvent = onChat(event.getPlayer(), event.getMessage(), event.getRecipients());
+        event.setCancelled(cancelEvent);
+    }
+
+    protected boolean onChat(Player talkingPlayer, String msg, Set<Player> recipients) {
+
+        boolean setCancelled = false;
+        if (FactionChat.useBanManager()) {
+            if (BanManagerAPI.isMuted(talkingPlayer.getName())) {
+                return setCancelled;
             }
         }
-        String msg = event.getMessage();
         //FPlayer me = (FPlayer)FPlayers.i.get(talkingPlayer);
         String chatmode = ChatMode.getChatMode(talkingPlayer);
         if (!chatmode.equalsIgnoreCase("PUBLIC")) {
@@ -108,108 +140,109 @@ public class FactionChatListener2 implements Listener {
             if (plugin.FactionsEnable) {
                 if (chatmode.equalsIgnoreCase("ALLY&TRUCE")) {
                     channel.fChatAT(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
 
                 } else if (chatmode.equalsIgnoreCase("ENEMY")) {
                     channel.fChatE(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 } else if (chatmode.equalsIgnoreCase("FACTION")) {
                     channel.fChatF(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 } else if (chatmode.equalsIgnoreCase("ALLY")) {
                     channel.fChatA(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 } else if (chatmode.equalsIgnoreCase("TRUCE")) {
                     channel.fChatTruce(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 } else if (chatmode.equalsIgnoreCase("LEADER")) {
                     channel.fChatLeader(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 } else if (chatmode.equalsIgnoreCase("OFFICER")) {
                     channel.fChatOfficer(talkingPlayer, msg);
-                    event.setCancelled(true);
+                    setCancelled = true;
                     isFactionChat = true;
                 }
 
                 if (isFactionChat) {
                     log.info("[FactionChat] " + chatmode + "|" + talkingPlayer.getName() + ": " + msg);
-                    return;
+                    return setCancelled;
                 }
 
             }
 
             if (chatmode.equalsIgnoreCase("VIP")) {
                 otherChannel.VIPChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             }
             if (chatmode.equalsIgnoreCase("UserAssistant")) {
                 otherChannel.userAssistantChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             } else if (chatmode.equalsIgnoreCase("JrMOD")) {
                 otherChannel.jrModChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             } else if (chatmode.equalsIgnoreCase("MOD")) {
                 otherChannel.modChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             } else if (chatmode.equalsIgnoreCase("SrMOD")) {
                 otherChannel.SrModChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             } else if (chatmode.equalsIgnoreCase("JrAdmin")) {
                 otherChannel.JrAdminChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             } else if (chatmode.equalsIgnoreCase("ADMIN")) {
                 otherChannel.adminChat(talkingPlayer, msg);
-                event.setCancelled(true);
+                setCancelled = true;
             }
 
             log.info("[FactionChat] " + chatmode + "|" + talkingPlayer.getName() + ": " + msg);
-        }
-        else
-        {
-            if (ChatMode.mutePublicOptionEnabled && !talkingPlayer.hasPermission("FactionChat.mutebypass"))
-            {
+        } else {
+            if (ChatMode.mutePublicOptionEnabled && !talkingPlayer.hasPermission("FactionChat.mutebypass")) {
                 for (final Player player : plugin.getServer().getOnlinePlayers()) {
                     if (ChatMode.IsPublicMuted(player)) {
-                        
-                        if (player.getName().equals(talkingPlayer.getName()))
-                        {
+
+                        if (player.getName().equals(talkingPlayer.getName())) {
                             ChatMode.MutePublicOption(player);
+                        } else {
+                            recipients.remove(player);
                         }
-                        else
-                        {
-                           event.getRecipients().remove(player); 
-                        }
-                        
+
                     }
                 }
             }
-            
-          
+
         }
-
-
+        return setCancelled;
     }
     
-    protected void onPlayerChatLocalOption(AsyncPlayerChatEvent event) {
-
+    private void onPlayerChatLocalOption_NonAsync(org.bukkit.event.player.PlayerChatEvent event) {
         if (event.isCancelled()) {
             return;
         }
+        onPlayerChatLocalOption(event.getPlayer(), event.getRecipients());
+    }
+    
+    protected void onPlayerChatLocalOption(org.bukkit.event.player.AsyncPlayerChatEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        onPlayerChatLocalOption(event.getPlayer(), event.getRecipients());
+    }
+
+    private void onPlayerChatLocalOption(Player player, Set<Player> recipients) {
         FileConfiguration config = this.plugin.getConfig();
-        Player player = event.getPlayer();
         Boolean localChatP = ChatMode.LocalChat.get(player.getName());
         if (!Objects.equals(localChatP, Boolean.TRUE)) {
-            
+
             if (!config.getBoolean("Features.LocalChat.Enable")) {
                 return;
             }
-            
+
             if (player.hasPermission("FactionChat.LocalChatBypass.PublicSend")) {
                 return;
             }
@@ -222,7 +255,7 @@ public class FactionChatListener2 implements Listener {
 
         for (Player player1 : this.plugin.getServer().getOnlinePlayers()) {
             if (FactionChatAPI.getDistance(player, player1) > MaxDistance && !player1.hasPermission("FactionChat.LocalChatBypass.PublicReceive")) {
-                event.getRecipients().remove(player1);
+                recipients.remove(player1);
             }
         }
     }
@@ -256,7 +289,7 @@ public class FactionChatListener2 implements Listener {
             }
         }
     }
-    
+
     private EventPriority setupEventPriority(String configNode) {
         int input = plugin.getConfig().getInt(configNode);
         if (input == 1) {
