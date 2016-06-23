22e276f18e450ec33de644743e2215c7353b1804
james137137 <james137137@gmail.com>
2016-06-24 11:17:16 +1200

    Fix for replaceall message containing $
    https://github.com/James137137/FactionChat/issues/29

--- src/main/java/nz/co/lolnet/james137137/FactionChat/FactionChatMessage.java
+++ src/main/java/nz/co/lolnet/james137137/FactionChat/FactionChatMessage.java
@@ -24,21 +24,21 @@ public class FactionChatMessage {
     String otherFactionName;
 
     public FactionChatMessage(String format, String message, boolean allowCostomColour, String playerPrefix, String playerName, String playerSuffix, String playerFactionTitle, String playerFactionRank, String FacitonName, String otherFactionName) {
-        this.format = format;
-        this.message = message;
+        this.format = checkFormat(format);
+        this.message = checkFormat(message);
         this.allowCostomColour = allowCostomColour;
-        this.playerPrefix = playerPrefix;
-        this.playerName = playerName;
-        this.playerSuffix = playerSuffix;
-        this.playerFactionTitle = playerFactionTitle;
-        this.playerFactionRank = playerFactionRank;
-        this.FacitonName = FacitonName;
-        this.otherFactionName = otherFactionName;
+        this.playerPrefix = checkFormat(playerPrefix);
+        this.playerName = checkFormat(playerName);
+        this.playerSuffix = checkFormat(playerSuffix);
+        this.playerFactionTitle = checkFormat(playerFactionTitle);
+        this.playerFactionRank = checkFormat(playerFactionRank);
+        this.FacitonName = checkFormat(FacitonName);
+        this.otherFactionName = checkFormat(otherFactionName);
     }
 
     public FactionChatMessage(String format, String message, boolean allowCostomColour) {
-        this.format = format;
-        this.message = message;
+        this.format = checkFormat(format);
+        this.message = checkFormat(message);
         this.allowCostomColour = allowCostomColour;
         this.playerPrefix = null;
         this.playerName = null;
@@ -87,11 +87,23 @@ public class FactionChatMessage {
             }
             
             format = format.replaceAll("  ", " "); //removing double spaces
+            message = message.replaceAll("\\$", "\\\\\\$");
             format = format.replaceAll("%MESSAGE%", message);
 
             output = format;
         }
 
+        return output;
+    }
+    
+    private static String checkFormat(String y)
+    {
+        if (y == null || y.length() == 0)
+        {
+            return y;
+        }
+        String output;
+        output = y.replaceAll("\\$", "\\\\\\$");
         return output;
     }
 
